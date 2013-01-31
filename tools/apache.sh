2d85c4126387d6a5d7fca28e7db4faccad243125
mtgreenway <mgreenway@uchicago.edu>
2013-01-31 12:22:01 -0600

    Removed tukey-portal stuff

--- tools/apache.sh
+++ tools/apache.sh
@@ -4,8 +4,10 @@
 # You will need to enable the sites after running this script.
 
 HOME_DIR=/home/ubuntu
+
 TUKEY_MIDDLEWARE=$HOME_DIR/tukey-middleware
-TUKEY_SITE=$HOME_DIR/tukey-site
+
+#TUKEY_SITE=$HOME_DIR/tukey-site
 APACHE_SITES_AVAILABLE=/etc/apache2/sites-available
 
 for site_name in auth glance nova 
@@ -13,4 +15,5 @@ do
 	ln -s $TUKEY_MIDDLEWARE/bin/${site_name}-apache.conf $APACHE_SITES_AVAILABLE/${site_name}
 done
 
-ln -s $TUKEY_SITE/tukey/openstack-dashboard.conf $APACHE_SITES_AVAILABLE/openstack-dashboard.conf
+# This action will be performed by the tukey-portal install.sh
+#ln -s $TUKEY_SITE/tukey/openstack-dashboard.conf $APACHE_SITES_AVAILABLE/openstack-dashboard.conf
