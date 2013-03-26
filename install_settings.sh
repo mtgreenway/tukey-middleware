2dc9e0281ca0867911a8de0d8380755a0d9262b6
mtgreenway <mgreenway@uchicago.edu>
2013-03-26 15:58:58 -0500

    added gpg script

--- install_settings.sh
+++ install_settings.sh
@@ -3,31 +3,33 @@
 # Somewhat sloppy but hopefully complete script for installing the 
 # tukey middleware.  Meant mostly for thourough documentation.
 
-declare -A PORTS=( ["auth"]=5000 ["nova"]=8774 ["glance"]=9292)
+# settings for running tukey on the same node as the cloud controller
+declare -A PORTS=( ["auth"]=5100 ["nova"]=8874 ["glance"]=9392)
 declare -A WSGI_DIRS=( ["auth"]="auth_proxy" ["nova"]="tukey_cli" ["glance"]="tukey_cli")
 
-PROXY_HOST="localhost"
+PROXY_HOST="127.0.0.2"
 
-CREATE_NEW_INTERFACE=false
+CREATE_NEW_INTERFACE=true
 CLONE_MIDDLEWARE=true
 
 TEMP_DIR=tukey-middleware
 
+#INSTALL_PRE=true
 INSTALL_PRE=true
 
-LOCAL_SETTINGS_FILE=/home/ubuntu/local_settings.py
-CONFIG_GEN_SETTINGS_FILE=/home/ubuntu/settings.py
-PGP_KEY_DIR=/home/ubuntu/keys
+CONFIG_BASE_DIR=/var/www/tukey/config/middleware
+LOCAL_SETTINGS_FILE=$CONFIG_BASE_DIR/local_settings.py
+CONFIG_GEN_SETTINGS_FILE=$CONFIG_BASE_DIR/settings.py
+PGP_KEY_DIR=$CONFIG_BASE_DIR/keys
 
-MIDDLEWARE_REPO=ssh://git@source.bionimbus.org/home/git/tukey-middleware.git
+MIDDLEWARE_REPO=http://git.bionimbus.org/git/tukey/tukey-middleware.git
 TEMP_DIR=tukey-middleware
 
 # make this be an absolute url
 MIDDLEWARE_DIR=/var/www/tukey/tukey-middleware
 
-#TUKEY_USER=tukey
-TUKEY_USER=ubuntu
-#TUKEY_GROUP=tukey
-TUKEY_GROUP=ubuntu
+TUKEY_USER=tukey
+TUKEY_GROUP=tukey
 
 APACHE_SITES_AVAILABLE=/etc/apache2/sites-available
+
