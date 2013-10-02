b26817d6eb25375a94918c3f12ea483e0274030b
mdabrowski <mdabro3@gmail.com>
2013-10-02 10:13:02 -0500

    Added a few things to the install.

--- install_tukey_middleware.sh
+++ install_tukey_middleware.sh
@@ -57,6 +57,10 @@ NameVirtualHost $PROXY_HOST:${PORTS[$site_name]}
 
 <Virtualhost $PROXY_HOST:${PORTS[$site_name]}>
 
+RewriteEngine On
+RewriteRule ^/sullivan/(.*) /$1 [PT]
+RewriteRule ^/goldberg/(.*) /$1 [PT]
+
 WSGIScriptAlias / $MIDDLEWARE_DIR/${WSGI_DIRS[$site_name]}/${site_name}_wsgi.py
 
 WSGIDaemonProcess tukey-$site_name user=$TUKEY_USER group=$TUKEY_GROUP processes=3 threads=1 python-path=$MIDDLEWARE_DIR/local:$MIDDLEWARE_DIR/${WSGI_DIRS[$site_name]}:$MIDDLEWARE_DIR/.venv/lib/python2.7/site-packages:$MIDDLEWARE_DIR/.venv/local/lib/python2.7/site-packages
